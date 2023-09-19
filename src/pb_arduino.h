14a0ae8c05822706c2d7ad3ffd0f4e2430b7ea55
Jonathan Haylett <jonathan@haylett.dev>
2023-09-19 01:11:03 +0100

    fix: ambiguity errors

--- src/pb_arduino.h
+++ src/pb_arduino.h
@@ -1,11 +1,15 @@
+#ifndef __PB_ARDUINO_H
+#define __PB_ARDUINO_H
+
 #include "pb_encode.h"
 #include "pb_decode.h"
 
-class Print;
-class Stream;
+#include <Arduino.h>
 
 //! Convert an object implementing Print into a nanopb ostream
 pb_ostream_s as_pb_ostream(Print& p);
 
 //! Convert an object implementing Print into a nanopb istream
 pb_istream_s as_pb_istream(Stream& p);
+
+#endif
