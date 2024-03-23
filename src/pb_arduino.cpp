7509db1a543574bab556a9bd32b3709f4dc53c48
Jonathan Haylett <jonathan@haylett.dev>
2024-03-23 15:46:17 +0000

    feat: allow setting initial bytes_left for istream

--- src/pb_arduino.cpp
+++ src/pb_arduino.cpp
@@ -1,30 +1,36 @@
 #include "pb_arduino.h"
+
 #include <Print.h>
 #include <Stream.h>
 
 static bool pb_print_write(pb_ostream_t *stream, const pb_byte_t *buf, size_t count) {
-    Print* p = reinterpret_cast<Print*>(stream->state);
+    Print *p = reinterpret_cast<Print *>(stream->state);
     size_t written = p->write(buf, count);
     return written == count;
 };
 
-pb_ostream_s as_pb_ostream(Print& p) {
-    return {pb_print_write, &p, SIZE_MAX, 0};
+pb_ostream_s as_pb_ostream(Print &p) {
+    return { pb_print_write, &p, SIZE_MAX, 0 };
 };
 
 static bool pb_stream_read(pb_istream_t *stream, pb_byte_t *buf, size_t count) {
-    Stream* s = reinterpret_cast<Stream*>(stream->state);
+    Stream *s = reinterpret_cast<Stream *>(stream->state);
     size_t read = s->readBytes(buf, count);
     if (read == 0) {
         stream->bytes_left = 0;
+        return false;
     }
     return read == count;
 };
 
-pb_istream_s as_pb_istream(Stream& s) {
+pb_istream_s as_pb_istream(Stream &s, size_t bytes_left) {
 #ifndef PB_NO_ERRMSG
-    return {pb_stream_read, &s, SIZE_MAX, 0};
+    return { pb_stream_read, &s, bytes_left, 0 };
 #else
-    return {pb_stream_read, &s, SIZE_MAX};
+    return { pb_stream_read, &s, bytes_left };
 #endif
+}
+
+pb_istream_s as_pb_istream(Stream &s) {
+    as_pb_istream(s, SIZE_MAX);
 };
--- src/pb_arduino.h
+++ src/pb_arduino.h
@@ -7,9 +7,10 @@
 #include <Arduino.h>
 
 //! Convert an object implementing Print into a nanopb ostream
-pb_ostream_s as_pb_ostream(Print& p);
+pb_ostream_s as_pb_ostream(Print &p);
 
-//! Convert an object implementing Print into a nanopb istream
-pb_istream_s as_pb_istream(Stream& p);
+//! Convert an object implementing Stream into a nanopb istream
+pb_istream_s as_pb_istream(Stream &p, size_t bytes_left);
+pb_istream_s as_pb_istream(Stream &p);
 
 #endif
